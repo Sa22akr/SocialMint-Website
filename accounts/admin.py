ff7bf037d6e42e2059737b0a39737727ab1d4167
Sa22akr <sa22akr@herts.ac.uk>
2026-02-19 11:46:27 +0000

    feat: enable Task and User management in Django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task, User
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "payout", "available", "platforms", "creator", "created_at")
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "balance", "earnings", "tasks_completed")
